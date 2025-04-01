b9ab5648713a74f9a17f12850fbb1f1b3e325757
Daniil Bogdanov <danya.bogdanov.03@mail.ru>
2025-04-01 20:52:14 +0300

    copro-code

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,11 @@
-use std::{fs::File, io::Read, ops::Index};
+use std::{fs::File, io::Read, rc::Rc, cell::RefCell};
+use std::collections::HashMap;
 
 enum NodeType {
     Country,
     Region,
-    District
+    District,
+    Root
 }
 
 enum StatesParser {
@@ -29,6 +31,46 @@ impl Node
     
         for child in &self.childs {
             child.print_struct(depth + 1);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct RuleNode {
+    name: String,
+    children: Vec<Rc<RefCell<RuleNode>>>,
+}
+
+impl RuleNode {
+    fn change_name(&mut self, new_name: &str) {
+        self.name = new_name.to_string();
+    }
+
+    // Конструктор нового узла
+    fn new(name: &str) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(
+            RuleNode {
+                name: name.to_string(),
+                children: Vec::new(),
+                }
+            )
+        )
+    }
+
+    // Добавление дочернего узла
+    fn add_child(parent: Rc<RefCell<Self>>, child: Rc<RefCell<Self>>) {
+        parent.borrow_mut().children.push(child);
+    }
+
+
+    fn parse_tree(node: &Rc<RefCell<RuleNode>>, depth: usize) {
+        let indent = "  ".repeat(depth); // Отступы по уровню вложенности
+        let node_borrowed = node.borrow();
+        
+        println!("{}- {}", indent, node_borrowed.name);
+        
+        for child in &node_borrowed.children {
+            RuleNode::parse_tree(child, depth + 1);
         }
     }
 }
@@ -78,8 +120,11 @@ fn check_struct(node: &Node) -> bool {
     is_valid
 }
 
+
 fn country_node(var_stack: & mut Vec<String>,
-                iter_lexem_table: & mut std::str::Split<'_, char>
+                iter_lexem_table: & mut std::str::Split<'_, char>,
+                rules_tree: Rc<RefCell<RuleNode>>,
+                last_node: & mut Rc<RefCell<RuleNode>>
                ) -> Result<Node, String>
 {
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
@@ -91,6 +136,9 @@ fn country_node(var_stack: & mut Vec<String>,
     {
         return Err("Must key word 'country'".to_string());
     }
+
+    let node_country = RuleNode::new("country");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&node_country));
 
     var_stack.push(iter_lexem[1].to_string());
 
@@ -104,6 +152,12 @@ fn country_node(var_stack: & mut Vec<String>,
         return Err("Must be delimeter like a ':' after 'country'".to_string());
     }
 
+    let delim = RuleNode::new("N");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&delim));
+
+    let delim_d = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&delim), Rc::clone(&delim_d));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                 .unwrap()
                                                 .split('-')
@@ -114,6 +168,12 @@ fn country_node(var_stack: & mut Vec<String>,
         return Err("Must be name of country in a String format".to_string());
     }
     
+    let C = RuleNode::new("C");
+    RuleNode::add_child(Rc::clone(&delim), Rc::clone(&C));
+
+    let node_name = RuleNode::new("name");
+    RuleNode::add_child(Rc::clone(&C), Rc::clone(&node_name));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
@@ -127,6 +187,12 @@ fn country_node(var_stack: & mut Vec<String>,
         return Err("Must be delimeter like a ':' after name of country".to_string());
     }
 
+    let A = RuleNode::new("A");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&A));
+
+    let delim = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&delim));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                 .unwrap()
                                                 .split('-')
@@ -137,11 +203,20 @@ fn country_node(var_stack: & mut Vec<String>,
         return Err("Must be Numeric value of area".to_string());
     }
 
+    let B = RuleNode::new("B");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&B));
+
+    let area_node = RuleNode::new("area");
+    RuleNode::add_child(Rc::clone(&B), Rc::clone(&area_node));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let area = var_stack.pop().unwrap().parse::<i64>().unwrap();
     let name = var_stack.pop().unwrap();
     let node_type = NodeType::Country;
+
+    area_node.borrow_mut().change_name(&area.to_string());
+    node_name.borrow_mut().change_name(&name);
 
     var_stack.pop();
 
@@ -154,14 +229,31 @@ fn country_node(var_stack: & mut Vec<String>,
         childs
     };
 
+    let D = RuleNode::new("D");
+    //let D = Rc::new(RefCell::new(RuleNode::new("D")));
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&D));
+
+    *last_node =  D; // Теперь last_node изменится и за пределами функции
+
     Ok(country_node)
 }
 
 fn region_node(var_stack: & mut Vec<String>,
                iter_lexem_table: & mut std::str::Split<'_, char>,
-               tree: & mut Node
+               tree: & mut Node,
+               rules_tree: Rc<RefCell<RuleNode>>,
+               last_node: &mut Rc<RefCell<RuleNode>>
               ) -> Result<i64, String>
 {
+    let d = RuleNode::new("|");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&d));
+
+    let R = RuleNode::new("R");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&R));
+
+    let region = RuleNode::new("region");
+    RuleNode::add_child(Rc::clone(&R), Rc::clone(&region));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                .unwrap()
                                                .split('-')
@@ -171,6 +263,9 @@ fn region_node(var_stack: & mut Vec<String>,
     {
         return Err("Must be delimeter like ':'".to_string());
     }
+
+    let N = RuleNode::new("N");
+    RuleNode::add_child(Rc::clone(&R), Rc::clone(&N));
 
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                .unwrap()
@@ -182,6 +277,15 @@ fn region_node(var_stack: & mut Vec<String>,
         return Err("Must be String value of region".to_string());
     }
 
+    let d1 = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&N), Rc::clone(&d1));
+
+    let C = RuleNode::new("C");
+    RuleNode::add_child(Rc::clone(&N), Rc::clone(&C));
+
+    let node_name = RuleNode::new("name");
+    RuleNode::add_child(Rc::clone(&C), Rc::clone(&node_name));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
@@ -194,6 +298,12 @@ fn region_node(var_stack: & mut Vec<String>,
         return Err("Must be delimeter like ':'".to_string());
     }
 
+    let A = RuleNode::new("A");
+    RuleNode::add_child(Rc::clone(&R), Rc::clone(&A));
+
+    let d1 = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&d1));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                .unwrap()
                                                .split('-')
@@ -204,11 +314,20 @@ fn region_node(var_stack: & mut Vec<String>,
         return Err("Must be numeric value of area".to_string());
     }
 
+    let B = RuleNode::new("B");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&B));
+
+    let area_node = RuleNode::new("area");
+    RuleNode::add_child(Rc::clone(&B), Rc::clone(&area_node));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let area = var_stack.pop().unwrap().parse::<i64>().unwrap();
     let name = var_stack.pop().unwrap();
     let node_type = NodeType::Region;
+
+    area_node.borrow_mut().change_name(&area.to_string());
+    node_name.borrow_mut().change_name(&name);
 
     let name_cp = name.clone();
 
@@ -224,6 +343,11 @@ fn region_node(var_stack: & mut Vec<String>,
     add_child(tree, region_node);
 
     let mut index = 0;
+
+    let D = RuleNode::new("D");
+    RuleNode::add_child(Rc::clone(&R), Rc::clone(&D));
+
+    *last_node = Rc::clone(&D); // Теперь last_node изменится и за пределами функции
 
     for element in & mut tree.childs
     {
@@ -241,9 +365,20 @@ fn region_node(var_stack: & mut Vec<String>,
 fn district_node(var_stack: & mut Vec<String>,
                  iter_lexem_table: & mut std::str::Split<'_, char>,
                  tree: & mut Node,
-                 node_index: i64
+                 node_index: i64,
+                 rules_tree: Rc<RefCell<RuleNode>>,
+                 last_node: & mut Rc<RefCell<RuleNode>>
                 ) -> Result<String, String>
 {
+    let d = RuleNode::new("|");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&d));
+
+    let E = RuleNode::new("E");
+    RuleNode::add_child(Rc::clone(&rules_tree), Rc::clone(&E));
+
+    let district = RuleNode::new("district");
+    RuleNode::add_child(Rc::clone(&E), Rc::clone(&district));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                 .unwrap()
                                                 .split('-')
@@ -253,6 +388,12 @@ fn district_node(var_stack: & mut Vec<String>,
     {
         return Err("Must be delimeter like ':'".to_string());
     }
+
+    let N = RuleNode::new("N");
+    RuleNode::add_child(Rc::clone(&E), Rc::clone(&N));
+
+    let d = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&N), Rc::clone(&d));
 
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
                                                 .unwrap()
@@ -264,6 +405,12 @@ fn district_node(var_stack: & mut Vec<String>,
         return Err("Must be String value of district".to_string());
     }
 
+    let C = RuleNode::new("C");
+    RuleNode::add_child(Rc::clone(&N), Rc::clone(&C));
+
+    let node_name = RuleNode::new("name");
+    RuleNode::add_child(Rc::clone(&C), Rc::clone(&node_name));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
@@ -276,6 +423,12 @@ fn district_node(var_stack: & mut Vec<String>,
         return Err("Must be delimeter like ':'".to_string());
     }
 
+    let A = RuleNode::new("A");
+    RuleNode::add_child(Rc::clone(&E), Rc::clone(&A));
+
+    let d1 = RuleNode::new(":");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&d1));
+
     let iter_lexem:Vec<&str> = iter_lexem_table.next()
         .unwrap()
         .split('-')
@@ -286,11 +439,20 @@ fn district_node(var_stack: & mut Vec<String>,
         return Err("Must be numeric value of area".to_string());
     }
 
+    let B = RuleNode::new("B");
+    RuleNode::add_child(Rc::clone(&A), Rc::clone(&B));
+    
+    let area_node = RuleNode::new("area");
+    RuleNode::add_child(Rc::clone(&B), Rc::clone(&area_node));
+
     var_stack.push(iter_lexem[1].to_string());
 
     let area = var_stack.pop().unwrap().parse::<i64>().unwrap();
     let name = var_stack.pop().unwrap();
     let node_type = NodeType::District;
+
+    area_node.borrow_mut().change_name(&area.to_string());
+    node_name.borrow_mut().change_name(&name);
 
     let childs:Vec<Node> = Vec::new();
 
@@ -300,6 +462,12 @@ fn district_node(var_stack: & mut Vec<String>,
         area,
         childs
     };
+
+    let D = RuleNode::new("D");
+    RuleNode::add_child(Rc::clone(&E), Rc::clone(&D));
+
+    *last_node = Rc::clone(&D); // Теперь last_node изменится и за пределами функции
+
 
     add_child(& mut tree.childs[node_index as usize], district_node);
 
@@ -330,7 +498,10 @@ pub fn syntax_parse(lexem_file:&str) -> Result<String, String>
     let mut iter_lexem_table: std::str::Split<'_, char> = buffer_lexems.split('\n');
     let mut cur_state:StatesParser = StatesParser::Start;
 
-    let mut tree = country_node(&mut var_stack, &mut iter_lexem_table).unwrap();
+    let mut rules_tree = RuleNode::new("S");
+    let mut last_node = Rc::clone(&rules_tree);
+
+    let mut tree = country_node(&mut var_stack, &mut iter_lexem_table, Rc::clone(&rules_tree), &mut last_node).unwrap();
     let mut node_index = 0;
 
     loop 
@@ -364,7 +535,7 @@ pub fn syntax_parse(lexem_file:&str) -> Result<String, String>
 
         if iter_lexem[0].to_string() == "String" && iter_lexem[1].to_string() == "region"
         {
-            match region_node(&mut var_stack, &mut iter_lexem_table, &mut tree)
+            match region_node(&mut var_stack, &mut iter_lexem_table, &mut tree, Rc::clone(&last_node), &mut last_node)
             {
                 Ok(node) => node_index = node,
                 Err(text) => {
@@ -373,9 +544,9 @@ pub fn syntax_parse(lexem_file:&str) -> Result<String, String>
                 }
             } ;
         }
-        else if iter_lexem[0].to_string() == "String" && iter_lexem[1].to_string() == "District" 
+        else if iter_lexem[0].to_string() == "String" && iter_lexem[1].to_string() == "district" 
         {
-            match district_node(&mut var_stack, &mut iter_lexem_table,  &mut tree , node_index)
+            match district_node(&mut var_stack, &mut iter_lexem_table,  &mut tree , node_index, Rc::clone(&last_node), &mut last_node)
             {
                 Ok(_) => continue,
                 Err(text) => {
@@ -385,16 +556,18 @@ pub fn syntax_parse(lexem_file:&str) -> Result<String, String>
             };
         }
         else {
-            return Err("Must key word 'region' or 'District'".to_string());
+            return Err("Must key word 'region' or 'district'".to_string());
         }
     }
 
-    tree.print_struct(0);
+    println!("Parse tree output");
+    RuleNode::parse_tree(&rules_tree, 0);
 
     match check_struct(&tree){
         true => println!("Structure is valid"),
         false => return Err("Structure is not valid".to_string())
     }
+
 
     Ok("OKi doki".to_string())
 }
